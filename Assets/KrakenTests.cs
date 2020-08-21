196e1bb5c16e34e16f4c25d2e83aeffcd217a3a2
Adam <achaysmail@gmail.com>
2020-08-21 18:25:53 -0500

    Objects now Spawnable

--- Assets/KrakenTests.cs
+++ Assets/KrakenTests.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class KrakenTests : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SpawnObject(GameObject objectToSpawn)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Spawn Object!");
     }
 }
